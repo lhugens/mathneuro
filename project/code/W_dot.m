70c9eda23d6c465148e0b3e40e0a664e82253e5c
lhugens <leonardolobato@protonmail.com>
2021-06-02 15:59:07 +0200

    IV curve for fixed points

---
+++ project/code/W_dot.m
@@ -0,0 +1,12 @@
+function W_dot = W_dot(V, W)
+    global E_L tau_W a;
+
+    % state variables
+    % V = u(1);
+    % W = u(2);
+
+    % time derivatives
+    % dudt    = zeros(2,1);
+    % dudt(1) = (I_app(t) - g_L * (V - E_L) + g_L * delta_T * exp((V - V_T)/delta_T) - W) / C;
+    W_dot = (a * (V - E_L) - W) / tau_W;
+end
